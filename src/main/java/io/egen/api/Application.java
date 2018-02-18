a5d5d96f11653a5682a1c5f72530a5c35bfc2243
kushangbhatt <kushangb@gmail.com>
2018-02-18 17:06:09 -0500

    updated with docker

--- src/main/java/io/egen/api/Application.java
+++ src/main/java/io/egen/api/Application.java
@@ -2,14 +2,17 @@ package io.egen.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
-
+//@Profile("prod")
 public class Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Starter of spring-boot app. Entry point
+		//System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "prod");
 		SpringApplication.run(Application.class, args);
 
 	}
